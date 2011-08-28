eb10a1f3b23f3082ed1603528e9768e934f5ab77
FaNiX <indoril69@gmail.com>
2011-08-28 14:37:36 +0300

    added remaining sms in gui

--- sms.py
+++ sms.py
@@ -33,7 +33,15 @@ class OtenetForm(QtGui.QWidget):
 
         self.message = ''
         self.number = ''
-        
+        self.content = self.parser()
+
+        try:
+            dailyLabel = QtGui.QLabel("Daily: "+ self.getDailyRemaining(self.content)+ "/5")
+            monthlyLabel = QtGui.QLabel("Monthly: "+ self.getMonthlyRemaining(self.content)+ "/100")
+        except:
+            QtGui.QMessageBox.critical(self, "Login Error", "Invalid Login Information.")
+            sys.exit(1)
+            
         phoneLabel = QtGui.QLabel("Phone:")
         self.phoneLine = QtGui.QLineEdit()
 
@@ -44,13 +52,17 @@ class OtenetForm(QtGui.QWidget):
         self.sendButton.show()
         self.exitButton = QtGui.QPushButton("&Exit")
         self.exitButton.show()
-
+          
         self.sendButton.clicked.connect(self.checkSMS)	
         self.exitButton.clicked.connect(self.exit_app)
 
         buttonLayout1 = QtGui.QHBoxLayout()
         buttonLayout1.addWidget(self.sendButton)
         buttonLayout1.addWidget(self.exitButton)
+        
+        labelLayout1 = QtGui.QHBoxLayout()
+        labelLayout1.addWidget(dailyLabel)
+        labelLayout1.addWidget(monthlyLabel)
 
         mainLayout = QtGui.QGridLayout()
         mainLayout.addWidget(phoneLabel, 0, 0)
@@ -58,6 +70,7 @@ class OtenetForm(QtGui.QWidget):
         mainLayout.addWidget(msgLabel, 1, 0, QtCore.Qt.AlignTop)
         mainLayout.addWidget(self.msgText, 1 ,1)
         mainLayout.addLayout(buttonLayout1, 3, 1)
+        mainLayout.addLayout(labelLayout1, 2 ,1, QtCore.Qt.AlignCenter)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("OtenetWebSMS Sender")
@@ -103,30 +116,35 @@ class OtenetForm(QtGui.QWidget):
 	  Send SMS
 	  (add error handling)
         """
+        
         msg.set_handle_robots(False)
         msg.open("http://tools.otenet.gr/tools/tiles/web2sms.do?showPage=smsSend&mnu=smenu23")
         msg.select_form(name="sendform")
         msg["phone"] = self.number
         msg["message"] = self.message
         msg.submit()
+        
         QtGui.QMessageBox.information(self, "Sent!", "Message was successfully sent.")
         print "Message succesfully sent."
-
-def parser():
-    """
-    WebPage Parser
-    """
-    resp = msg.open("http://tools.otenet.gr/tools/tiles/web2sms.do?showPage=smsSend&mnu=smenu23")
-    content = str(resp.read())
-    return content
-    
-def getDailyRemaining(content):
-    content = re.search(r'<input type="hidden" name="todaySMS" value="(\d+)">', content)
-    return content.group(1)
-    
-def getMonthlyRemaining(content):
-    content = re.search(r'<input type="hidden" name="monthSMS" value="(\d+)">', content)
-    return content.group(1)
+        
+        self.dailyLabel.setText(self, "Daily: "+ self.getDailyRemaining(self.content)+ "/5")
+        self.monthlyLabel.setText(self, "Monthly: "+ self.getMonthlyRemaining(self.content)+ "/100")
+        
+    def parser(self):
+        """
+        WebPage Parser
+        """
+        resp = msg.open("http://tools.otenet.gr/tools/tiles/web2sms.do?showPage=smsSend&mnu=smenu23")
+        content = str(resp.read())
+        return content
+        
+    def getDailyRemaining(self,content):
+        content = re.search(r'<input type="hidden" name="todaySMS" value="(\d+)">', content)
+        return content.group(1)
+        
+    def getMonthlyRemaining(self,content):
+        content = re.search(r'<input type="hidden" name="monthSMS" value="(\d+)">', content)
+        return content.group(1)
 
 def login_ote(user, password):
     """
@@ -143,11 +161,6 @@ def login_ote(user, password):
     
 if __name__ == '__main__':
     login_ote('***','***')		#login(username, password)
-    
-    content = parser()
-    getDailyRemaining(content)
-    getMonthlyRemaining(content)
-    
     app = QtGui.QApplication(sys.argv)
     otenet_form = OtenetForm()
     otenet_form.show()
