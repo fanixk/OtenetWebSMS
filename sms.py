07e2da7a7755189de99f4fec619831248cb62fbf
FaNiX <indoril69@gmail.com>
2011-09-21 12:57:37 +0300

    gpl

--- sms.py
+++ sms.py
@@ -2,7 +2,7 @@
 
 #############################################################################
 #    									    #
-#    Copyright (C) 2011 FaNiX                                 #
+#    Copyright (C) 2011 FaNiX                                               #
 #			                                                    #
 #    This program is free software: you can redistribute it and/or modify   #
 #    it under the terms of the GNU General Public License as published by   #
