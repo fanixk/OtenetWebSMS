daba9f4a2fd21c84936fc0bdd20d8cd69534f8c5
FaNiX <indoril69@gmail.com>
2011-09-21 12:57:12 +0300

    gpl

--- sms.py
+++ sms.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 #############################################################################
-#									    #				
+#    									    #
+#    Copyright (C) 2011 FaNiX                                 #
+#			                                                    #
 #    This program is free software: you can redistribute it and/or modify   #
 #    it under the terms of the GNU General Public License as published by   #
 #    the Free Software Foundation, either version 3 of the License, or	    #
